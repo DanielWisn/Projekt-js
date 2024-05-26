b6ea4f5dd0ea16e9d582c55494a0d6a0f0e022c0
DanielWisn <u145_danwis_waw@technischools.com>
2024-05-26 22:19:14 +0200

    Koniec

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
+const Board = document.querySelector("div#board")
 const fields = document.querySelectorAll("div.field");
 const spans = document.querySelectorAll("span");
 const h2 = document.querySelector("h2");
 
-let gameEnd = false;
+let gameEnd = 0;
+
+let board = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
 
 const handleFieldClick = (event) => {
+  if (gameEnd === 9){
+    return;
+  }
   const clickedField = event.target;
   const randomNum = Math.floor(Math.random() * 9);
   const span = clickedField.querySelector("span");
+  const index = clickedField.getAttribute("data-index");
   if (!span.textContent) {
     span.textContent = "X";
     clickedField.classList.add("player");
-    generateRandomField(randomNum);
+    board[index[0]][index[2]] = span.textContent;
+    gameEnd += 1
+    findWinner(board);
+    console.log(findWinner(board));
+    if (checkDraw() === true){
+      return;
+    }
+    else{
+      setTimeout(generateRandomField,1000,randomNum);
+      findWinner(board);
+    }
   } else {
     return;
   }
 };
 
 const generateRandomField = (random) => {
-  const randomSpan = fields[random].querySelector("span")
-  const randomField = fields[random]
-  if (
-    randomSpan.textContent !== "O" &&
-    randomSpan.textContent !== "X"
-  ) {
-
-    fields[random].classList.add("robot");
-    randomSpan.textContent = "O";
-  } else {
-    random = Math.floor(Math.random() * (9 - 1) + 1);
-    generateRandomField(random);
+  if (gameEnd === 9){
+    return;
+  }
+  else if (gameEnd < 9){
+    const randomSpan = fields[random].querySelector("span");
+    const randomField = fields[random];
+    const randomIndex = randomField.getAttribute("data-index");
+    if (
+      randomSpan.textContent !== "O" &&
+      randomSpan.textContent !== "X"
+    ) {
+      randomField.classList.add("robot");
+      randomSpan.textContent = "O";
+      board[randomIndex[0]][randomIndex[2]] = randomSpan.textContent;
+      gameEnd += 1
+    } else {
+      random = Math.floor(Math.random() * (9));
+      generateRandomField(random);
+    }
   }
 };
 
-fields.forEach((field) => {
-  field.addEventListener("click", handleFieldClick);
-});
-
-przyklad = [
-  ["O", undefined, "X"],
-  ["O", "X", undefined],
-  ["X", undefined, "O"],
-];
-
 const findWinner = function (game) {
-  const czyskonczone = 0;
   const checkWinner = [
     (Row1 = [game.at(0)[0], game.at(0)[1], game.at(0)[2]]),
-    (Row2 = [game.at(0)[0], game.at(1)[1], game.at(2)[2]]),
-    (Row3 = [game.at(0)[0], game.at(1)[1], game.at(2)[2]]),
+    (Row2 = [game.at(1)[0], game.at(1)[1], game.at(1)[2]]),
+    (Row3 = [game.at(2)[0], game.at(2)[1], game.at(2)[2]]),
     (column1 = [game.at(0)[0], game.at(1)[0], game.at(2)[0]]),
     (column2 = [game.at(0)[1], game.at(1)[1], game.at(2)[1]]),
     (column3 = [game.at(0)[2], game.at(1)[2], game.at(2)[2]]),
     (diagonal1 = [game.at(0)[0], game.at(1)[1], game.at(2)[2]]),
-    (diagonal2 = [game.at(0)[2], game.at(1)[1], game.at(0)[2]]),
+    (diagonal2 = [game.at(2)[0], game.at(1)[1], game.at(0)[2]]),
   ];
   for (let i in checkWinner) {
     if (
@@ -61,55 +78,36 @@ const findWinner = function (game) {
       checkWinner[i].at(1) === "X" &&
       checkWinner[i].at(2) === "X"
     ) {
-      fields.forEach((field) => {
-        field.classList.add("player");
-      });
-      h2.textContent = "X";
-      return "X";
+      h2.textContent = "X wins";
+      h2.classList.add("player");
+      gameEnd = 9;
+      return "X"
     } else if (
       checkWinner[i].at(0) === "O" &&
       checkWinner[i].at(1) === "O" &&
       checkWinner[i].at(2) === "O"
     ) {
-      fields.forEach((field) => {
-        field.classList.add("robot");
-      });
-      h2.textContent = "O";
-      return "O";
+      h2.textContent = "O wins";
+      h2.classList.add("robot");
+      gameEnd = 9;
+      return "O"
     }
-  }
-  for (let i in checkWinner) {
-    if (
-      checkWinner[i].at(0) === undefined ||
-      checkWinner[i].at(1) === undefined ||
-      checkWinner[i].at(2) === undefined
-    ) {
-      czyskonczone = false;
-    }
-  }
-  if (czyskonczone === false) {
-    return undefined;
-  } else {
-    return null;
   }
 };
 
-while (gameEnd === false) {
-  let amountOfMoves = 0;
-
-  board = [
-    [spans[0].textContent, spans[1].textContent, spans[3].textContent],
-    [spans[4].textContent, spans[5].textContent, spans[6].textContent],
-    [spans[7].textContent, spans[8].textContent, spans[9].textContent],
-  ];
-  for (i in board) {
-    for (j in board[i]) {
-      if (j.textContent === "X" || j.textContent === "O") {
-        amountOfMoves += 1;
-      }
-    }
-  }
-  if (amountOfMoves >= 3) {
-    findWinner(board);
+const checkDraw = function () {
+  if (
+    gameEnd === 9 &&
+    (findWinner(board) !== "X" &&
+    findWinner(board) !== "O")
+  ) {
+    h2.classList.add("draw")
+    h2.textContent = "Draw"
+    return true;
   }
 }
+
+fields.forEach((field) => {
+  field.addEventListener("click", handleFieldClick);
+});
+
